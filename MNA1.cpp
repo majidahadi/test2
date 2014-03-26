63e66625699e6dae55d732edb8ac0c431fd7c2cb
majidahadi <majid.ahadi09@gmail.com>
2014-03-26 12:00:41 -0600

    cfile

---
+++ MNA1.cpp
@@ -0,0 +1,20 @@
+// MNA1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>   /* required for file operations */
+#include <conio.h>  /* for clrscr */
+#include <dos.h>  /* for delay */
+
+FILE *fr; 
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int n;
+    long elapsed_seconds;
+    char line[80];
+
+
+	return 0;
+}
+
